f3eac8eec87d4ef73d779057899bf7c56695003b
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2025-01-02 15:27:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -0,0 +1,56 @@
+#User function Template for python3
+from collections import defaultdict
+class Solution:
+    def countSubarrays(self, arr, k):
+        # code here
+        mp = defaultdict(int)   # [10, 2, -2, -20, 10], -10
+        res = 0
+        cur = 0
+        for v in arr:
+            cur += v
+            if cur == k:
+                res += 1
+    
+            if cur - k in mp:
+                res += mp[cur-k]
+    
+            mp[cur] += 1
+    
+    
+        return res
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        k = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        res = ob.countSubarrays(arr, k)
+        print(res)
+        print("~")
+
+# } Driver Code Ends
