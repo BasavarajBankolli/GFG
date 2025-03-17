edac62c4395b36d522d99995dc2170af99758a78
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-03-17 23:46:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -0,0 +1,26 @@
+from functools import reduce
+class Solution:
+    def isSubsetSum (self, arr, su):
+        return (reduce(lambda x, y: x|(x<<y), [1]+arr) & (1<<su)) > 0
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        arr = input().split()
+        N = len(arr)
+        for itr in range(N):
+            arr[itr] = int(arr[itr])
+        sum = int(input())
+
+        ob = Solution()
+        if ob.isSubsetSum(arr, sum) == True:
+            print("true")
+        else:
+            print("false")
+
+        print("~")
+
+# } Driver Code Ends
