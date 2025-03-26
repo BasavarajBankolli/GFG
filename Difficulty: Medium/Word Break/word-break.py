e86478f7a519c829db0a5f9575734e023ae4b17e
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-03-27 01:11:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Break/word-break.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordBreak(self, s, words):
+        dp = [True] + [False] * len(s)
+
+        for i in range(1, len(s) + 1):
+            for w in words:
+                start = i - len(w)
+                if start >= 0 and dp[start] and s[start:i] == w:
+                    dp[i] = True
+                    break
+        
+        return dp[-1]
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    test_case = int(input())
+
+    for _ in range(test_case):
+        s = input().strip()
+        dictionary = input().strip().split()
+        ob = Solution()
+        res = ob.wordBreak(s, dictionary)
+        if res:
+            print("true")
+        else:
+            print("false")
+        print("~")
+# } Driver Code Ends
