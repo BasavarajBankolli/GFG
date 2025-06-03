5dc776607717a9281dda5378bbf969f9085bb917
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-06-03 13:29:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -1,54 +1,28 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    int atMostK(string& s, int k) {
-        int a[26] = {0};
-        int res = 0, n = s.size();
-        int cnt = 0;
-        for (int i = 0, j = 0; i < n; i++) {
-            if (a[s[i] - 'a']++ == 0) cnt++;
+    int atMost(string &s, int k){
+        int freq[26] = {0};
+        int res = 0;
+        int i = 0, distinct = 0;
+        
+        for (int j = 0; j < s.length(); j++) {
+            if (freq[s[j] - 'a'] == 0) distinct++;
             
-            while (cnt > k) {
-                if (--a[s[j] - 'a'] == 0) cnt--;
-                j++;
+            freq[s[j]-'a']++;
+            
+            while(distinct > k){
+                freq[s[i]-'a']--;
+                if (freq[s[i] - 'a'] == 0) distinct--;
+                i++;
             }
             
-            res += (i - j + 1);
+            res += j - i + 1;
+            
         }
+        
         return res;
     }
-    
     int countSubstr(string& s, int k) {
-        return atMostK(s, k) - atMostK(s, k - 1);
+        return atMost(s, k) - atMost(s, k-1);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        int k;
-        cin >> k;
-        Solution ob;
-        cout << ob.countSubstr(s, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-// } Driver Code Ends
